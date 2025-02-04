a87b86779541e534ecc3a9fc915fe6d7c2a8e4d0
Shrey0610 <shreyshah241@gmail.com>
2025-02-04 21:41:53 +0530

    adjusted deploying to github pagess

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Route, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DisplayGrid from './components/grid/grid';
 
 export default function App() {
   return (
-    <HashRouter>
-      <Route path="/" element={<DisplayGrid />} exact />
-    </HashRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<DisplayGrid />} exact />
+      </Routes>
+    </Router>
   );
 }
